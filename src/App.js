bec7f546dcc89e395749a474a21e41cbd4169543
this-is-pooja <poojasoliwal2222@gmail.com>
2022-01-26 18:20:58 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { BiHide } from "react-icons/bi";
+import { BiShow } from "react-icons/bi";
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const styleIcon = { color: "#36454f", fontSize: "1.1rem" };
+  const [query, setQuery] = useState("");
+  const [password, setPassword] = useState("");
+  const [login, setLogin] = useState(false);
+  const [token, setToken] = useState(null);
+  const [passwordShown, setPasswordShown] = useState(false);
+  const togglePassword = () => {
+    setPasswordShown(prevState => !prevState);
+  };
+  const Login = () => {
+    fetch('https://myphysio.digitaldarwin.in/api/login/', {
+      method: 'POST',
+      body: JSON.stringify({ "uid": `${query}`, "password": `${password}`, "blocked": 0 })
+    }).then((res) => {
+      res.json().then((result) => {
+        console.log(result);
+        localStorage.setItem('login', JSON.stringify({
+          login: true,
+          token: result.token
+        }))
+        setLogin(true)
+      })
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='phy-head'>PHYSIOAI</h1>
+      <h2>Welcome Back!</h2>
+      <form>
+        <label className='label'>
+          <span class="required">*</span>Username
+        </label>
+        <input type="text" className="input" placeholder=''
+          value={query}
+          onChange={e => setQuery(e.target.value)} />
+        <label className='label'>
+          <span class="required">*</span>Password
+        </label>
+        <div className="password-control">
+          <input
+            className="password-filled"
+            type={passwordShown ? "text" : "password"}
+            placeholder=''
+            value={password}
+            onChange={e => setPassword(e.target.value)} />
+          <button className="btn" onClick={togglePassword}>
+            {
+              passwordShown ? <BiHide style={styleIcon} /> : <BiShow style={styleIcon} />
+            }
+          </button>
+        </div>
+      </form>
+      <h3>Forgot Password?</h3>
+      <button className="log-btn" onClick={Login}>Login</button>
     </div>
   );
 }
